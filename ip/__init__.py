223918c1037a6e89993bb953903d042117416c8d
Navneet Thillaisthanam <navneet.ts@gmail.com>
2020-03-01 23:14:01 +0530

    First commit

---
+++ ip/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import json
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    headers = req.headers['x-forwarded-for']
+
+    if headers:
+        return func.HttpResponse(json.dumps({"public_ip": headers.split(':')[0]}))
+    else:
+        return func.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400
+        )
